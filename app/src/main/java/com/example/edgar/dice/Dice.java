b2d91b1b6a005c2d025ff079bd2486362cfe3657
Eggertron <bboyhan@hotmail.com>
2017-08-19 23:44:50 -0400

    forced portrait mode. added launch icon. finished Dice class.

---
+++ app/src/main/java/com/example/edgar/dice/Dice.java
@@ -0,0 +1,29 @@
+package com.example.edgar.dice;
+
+import java.util.Random;
+
+/**
+ * Created by edgar on 8/19/17.
+ */
+
+public class Dice {
+    private Random random;
+    private int sides;
+
+    public Dice() {
+        init(6);
+    }
+
+    public Dice(int sides) {
+        init(sides);
+    }
+
+    private void init(int sides) {
+        random = new Random();
+        this.sides = sides;
+    }
+
+    public int roll() {
+        return random.nextInt(sides) + 1;
+    }
+}
